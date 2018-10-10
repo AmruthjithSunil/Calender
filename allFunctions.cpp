5c46fa51c02be14df6b76cff9beae938c737ca8e
Amruthjith Sunil <amruthjithsunil@gmail.com>
2018-10-10 19:38:27 +0530

    implemented Functions

---
+++ allFunctions.cpp
@@ -0,0 +1,42 @@
+void computeYear() {
+    year = year % 400;
+    c = year + year/4;
+}
+
+void computeMonth() {
+    switch(month){
+        case 1: c+=6; break;
+        case 2: c+=2; break;
+        case 3: c+=2; break;
+        case 4: c+=5; break;
+        case 5: c+=0; break;
+        case 6: c+=3; break;
+        case 7: c+=5; break;
+        case 8: c+=1; break;
+        case 9: c+=4; break;
+        case 10: c+=6; break;
+        case 11: c+=2; break;
+        case 12: c+=4; break;
+    }
+}
+
+void computeException() {
+    if(month < 3 && year%4 == 0){
+        c--;
+    }
+    c -= year/100;
+}
+
+void display() {
+    cout << "\t";
+    switch(c % 7){
+        case 0: cout<<"Sun"; break;
+        case 1: cout<<"Mon"; break;
+        case 2: cout<<"Tues"; break;
+        case 3: cout<<"Wednes"; break;
+        case 4: cout<<"Thurs"; break;
+        case 5: cout<<"Fri"; break;
+        case 6: cout<<"Satur"; break;
+    }
+    cout<<"day\n";
+}
--- calender.cpp
+++ calender.cpp
@@ -1,41 +1,24 @@
+/*
+    This is a calender program.
+    Developer : Amruthjith Sunil
+*/
+
+
 #include<iostream>
 
 using namespace std;
 
+unsigned c, year, month, day;
+
+#include "./allFunctions.cpp"
+
 int main(){
-    unsigned c, y, m, d;
     cout<<"Enter the date : ";
-    cin>>d>>m>>y;
-    y = y % 400;
-    c = y + y/4;
-    switch(m){
-        case 1: c+=6; break;
-        case 2: c+=2; break;
-        case 3: c+=2; break;
-        case 4: c+=5; break;
-        case 5: c+=0; break;
-        case 6: c+=3; break;
-        case 7: c+=5; break;
-        case 8: c+=1; break;
-        case 9: c+=4; break;
-        case 10: c+=6; break;
-        case 11: c+=2; break;
-        case 12: c+=4; break;
-    }
-    if(m < 3 && y%4 == 0){
-        c--;
-    }
-    c -= y/100;
-    c += d;
-    switch(c % 7){
-        case 0: cout<<"Sun"; break;
-        case 1: cout<<"Mon"; break;
-        case 2: cout<<"Tues"; break;
-        case 3: cout<<"Wednes"; break;
-        case 4: cout<<"Thurs"; break;
-        case 5: cout<<"Fri"; break;
-        case 6: cout<<"Satur"; break;
-    }
-    cout<<"day\n";
+    cin >> day >> month >> year;
+    computeYear();
+    computeMonth();
+    computeException();
+    c += day;
+    display();
     return 0;
 }
